bdc95bf9ac92b83a8cca7bb9e9416473265e7b1c
JavacLMD <dorscherl@gmail.com>
2026-03-26 22:27:59 -0500

    Added Week 2 Discussion project

---
+++ Week 2/M2-Discussion/M2-Discussion.cpp
@@ -0,0 +1,24 @@
+// M2-Discussion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Create a simple C++ program using input/output processes and at least two errors. The program should not execute and there should be at least two errors in the code.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string name;
+int age
+
+int main()
+{
+    cout << "Hello World! What is your name? \n";
+	cin >> name;
+
+	cout << "How old are you? \n";
+	cin >> age;
+
+	cout << "Hello " << name < ", you are " << age << " years old! \n";
+
+
+}
